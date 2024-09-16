2e836239c4ced09e16b971144906dc58551c918a
Parth Chittora <109902371+ParthChittora@users.noreply.github.com>
2024-09-16 11:50:22 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        String jsonInput = "{\"keys\": {\"n\": 4, \"k\": 3}, \"1\": {\"base\": \"16\", \"value\": \"4\"}, \"2\": {\"base\": \"2\", \"value\": \"111\"}, \"3\": {\"base\": \"16\", \"value\": \"12\"}, \"6\": {\"base\": \"4\", \"value\": \"213\"}}";
+        Map<Integer, BigInteger> shares = parseInput(jsonInput);
+        BigInteger secret = reconstructSecret(shares, 3);  // k = 3
+        System.out.println("The reconstructed secret is: " + secret);
+    }
+    }
